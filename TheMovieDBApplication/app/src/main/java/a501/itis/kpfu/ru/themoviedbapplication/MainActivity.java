1ce2d43fe239a125e829c0f25be65c5239980554
Amir Kadyrov <amir.kadyrov.97@mail.ru>
2017-01-12 19:54:53 +0300

    Blank for app

---
+++ TheMovieDBApplication/app/src/main/java/a501/itis/kpfu/ru/themoviedbapplication/MainActivity.java
@@ -0,0 +1,39 @@
+package a501.itis.kpfu.ru.themoviedbapplication;
+
+import android.os.Bundle;
+import android.support.annotation.IdRes;
+import android.support.v7.app.AppCompatActivity;
+
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnTabSelectListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomBar bottomBar = (BottomBar) findViewById(R.id.bottomBar);
+        bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
+            @Override
+            public void onTabSelected(@IdRes int tabId) {
+                if (tabId == R.id.tab_movies) {
+
+                }
+                if (tabId == R.id.tab_movies_second) {
+
+                }
+                if (tabId == R.id.tab_main) {
+
+                }
+                if (tabId == R.id.tab_series) {
+
+                }
+                if (tabId == R.id.tab_series_second) {
+
+                }
+            }
+        });
+    }
+}
