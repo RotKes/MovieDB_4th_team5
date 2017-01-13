a32f1727151eeea9a566f2ff7af1158c0b7758e0
Marat Khusnullin <55klevanso55@gmail.com>
2017-01-13 17:49:48 +0300

    start tv series work

---
+++ TheMovieDBApplication/app/src/main/java/a501/itis/kpfu/ru/themoviedbapplication/fragments/TvSerialsListFragment.java
@@ -0,0 +1,19 @@
+package a501.itis.kpfu.ru.themoviedbapplication.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import a501.itis.kpfu.ru.themoviedbapplication.R;
+
+/**
+ * Created by Марат on 13.01.2017.
+ */
+
+public class TvSerialsListFragment extends Fragment {
+
+
+
+}
